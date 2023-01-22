fc485ced54bd498bf6f26c63ad9002dfb947cd50
AbdalaMire <121616854+AbdalaMire@users.noreply.github.com>
2023-01-22 20:42:30 +0300

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,22 @@
+public class MatrixAddition {
+    public static void main(String[] args) {
+        int[][] matrixA = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrixB = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+
+        int[][] result = new int[matrixA.length][matrixA[0].length];
+
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[0].length; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        System.out.println("Matrix addition:");
+        for (int[] row : result) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
